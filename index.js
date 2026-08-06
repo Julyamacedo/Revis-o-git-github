b859f1fd747fd88ae3d1f76cd92d5183bee724b7
julyamacedo <julya.macedo@edu.senai.br>
2026-08-06 14:47:59 -0300

    feat: adiciona lista de produtos

---
+++ index.js
@@ -0,0 +1,8 @@
+//nome:Julya macedo
+//nome:katherine de Paula
+//nome:Isabela Rossi
+//PRODUTO
+let produto = ['shampoo', 'condicionador', 'mascara', 'finalização'];
+console.log("Lista de produtos inicial");
+console.log("=========================");
+console.table(produto);
