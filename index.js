651aaf9d1446c13d698130fb49b5cb5fdd1b69d1
julyamacedo <julya.macedo@edu.senai.br>
2026-08-06 15:57:29 -0300

    feat :localizar registros

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 //nome:Julya macedo
 //nome:katherine de Paula
 //nome:Isabela Rossi
-//PRODUTO
-let produto = ['shampoo', 'condicionador', 'mascara', 'finalização'];
-console.log("Lista de produtos inicial");
-console.log("=========================");
-console.table(produto);
+
+//PRODUTOS DE CABELOS - DESENVOLVIMENTO 1
+let produtos = ['shampoo', 'condicionador', 'mascara', 'finalização'];
+console.log('* LISTA DE PRODUTO INICIAL');
+console.log('===========================');
+console.table(produtos);
+
+console.log('* REMOVER PRODUTO INICIAL');
+console.log('===========================');
+produtos.shift();
+console.table(produtos);
+
+console.log('* ADICIONAR NOVO PRODUTO INICIAL');
+console.log('===========================');
+produtos.unshift('shampoo a seco');
+console.table(produtos);
+
+console.log('* PROCURAR PRODUTO');
+console.log('===========================');
+console.log(produtos.find((produto) => produto === 'condicionador'));
+console.log('----------------------------')
+
+ // .
