218a65fb897c72de1ca86b6bfdd70ee324b33eaa
julyamacedo <julya.macedo@edu.senai.br>
2026-08-06 14:29:38 -0300

    feat:remove o primeiro registro

---
+++ index.js
@@ -0,0 +1,7 @@
+//nome:Julya macedo
+//nome:katherine de Paula
+//nome:Isabela Rossi
+//PRODUTO
+let produto = ["shampoo", "condicionador", "mascara", "finalização"];
+produto.shift();
+console.table(produto);
